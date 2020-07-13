da48bb5d856ed6bd1c33e6b3e91aa476a7c114ec
ShinyCode <ShinyCode@users.noreply.github.com>
2020-07-12 23:54:49 -0700

    Move out non-essentials into util

--- natural/chexpeditor_client.py
+++ natural/chexpeditor_client.py
@@ -1,47 +1,17 @@
 import cv2
-import numpy as np
-import pandas as pd
 
 from pathlib import Path
-from PIL import Image
 
-from natural.util import add_background
+from natural.util import load_data, display_img
 
 WINDOW_NAME = 'CheXpeditor Client'
-COL_PATH = 'Path'
 ESC_CODE = 27
-
-
-def load_data(csv_path, data_dir, row_start, row_end):
-    assert Path(csv_path).exists()
-    # Load image paths from CSV and resolve relative to data_dir
-    df = pd.read_csv(csv_path)
-    img_paths = list(map(lambda path: data_dir / path, df[COL_PATH]))
-    # Select images based on given range
-    if row_end is None:
-        img_paths = img_paths[row_start:]
-    else:
-        img_paths = img_paths[row_start: row_end]
-    # Check that all images in range exist
-    for img_path in img_paths:
-        assert img_path.exists(), f"Could not locate image {str(img_path)}!"
-    return img_paths
-
-
-def display_img(img_path, screen_width, screen_height, i=None, total=None):
-    progress = f'[{i + 1}/{total}] ' if i is not None and total is not None else ''
-    print(f'{progress}Showing image {str(img_path)}...')
-    # Need to convert back to str for compatibility with Win-SSHFS
-    img = Image.open(str(img_path)).convert('RGB')
-    img = add_background(img, screen_width, screen_height)
-    img = np.array(img)[..., ::-1]
-    cv2.imshow(WINDOW_NAME, img)
 
 
 def run_manual(img_paths, screen_width, screen_height, delay):
     total = len(img_paths)
     for i, img_path in enumerate(img_paths):
-        display_img(img_path, screen_width, screen_height, i=i, total=total)
+        display_img(WINDOW_NAME, img_path, screen_width, screen_height, i=i, total=total)
         if cv2.waitKey(delay) == ESC_CODE:
             break
 
--- natural/util.py
+++ natural/util.py
@@ -1,5 +1,11 @@
 import numpy as np
 from PIL import Image
+from pathlib import Path
+import pandas as pd
+import cv2
+
+COL_PATH = 'Path'
+
 
 def add_background(img, out_width, out_height):
     """Paste image onto a black background with the same size as screen.
@@ -29,3 +35,29 @@ def add_background(img, out_width, out_height):
     paste_y = (out_height - new_height) // 2
     background.paste(img, (paste_x, paste_y))
     return background
+
+
+def load_data(csv_path, data_dir, row_start, row_end):
+    assert Path(csv_path).exists()
+    # Load image paths from CSV and resolve relative to data_dir
+    df = pd.read_csv(csv_path)
+    img_paths = list(map(lambda path: data_dir / path, df[COL_PATH]))
+    # Select images based on given range
+    if row_end is None:
+        img_paths = img_paths[row_start:]
+    else:
+        img_paths = img_paths[row_start: row_end]
+    # Check that all images in range exist
+    for img_path in img_paths:
+        assert img_path.exists(), f"Could not locate image {str(img_path)}!"
+    return img_paths
+
+
+def display_img(window_name, img_path, screen_width, screen_height, i=None, total=None):
+    progress = f'[{i + 1}/{total}] ' if i is not None and total is not None else ''
+    print(f'{progress}Showing image {str(img_path)}...')
+    # Need to convert back to str for compatibility with Win-SSHFS
+    img = Image.open(str(img_path)).convert('RGB')
+    img = add_background(img, screen_width, screen_height)
+    img = np.array(img)[..., ::-1]
+    cv2.imshow(window_name, img)
